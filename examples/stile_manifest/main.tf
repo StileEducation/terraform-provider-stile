ef02f86ec0760354e06e88a4ad4912752aae1f86
Nick Spain <nicholas.spain96@gmail.com>
2023-03-07 13:19:19 +1100

    Update example to use fallback

--- examples/stile_manifest/main.tf
+++ examples/stile_manifest/main.tf
@@ -1,15 +1,34 @@
 terraform {
   required_providers {
     stile = {
-      versions = ["0.2"]
+      version = "0.2"
       source = "hashicorp.com/edu/stile"
     }
   }
 }
 
 data "stile_manifest" "all" {
-  bfp_build_number = 470011
-  manifest_name = "untested-excel-exporter-manifest.json"
+  bfp_build_number = 926993
+  manifest_name = "untested-prober-service-manifest.json"
+  fallback_manifest = jsonencode({
+    "name": "926993",
+    "commits": {
+      "git@github.com:StileEducation/dev-environment.git": "a commit"
+    },
+    "service_versions": {
+      "stile-prober": "stile-prober",
+      "stile-prometheus-node-exporter": "stile-prometheus-node-exporter",
+      "prober-service": "prober-service",
+      "stile-consul": "stile-consul",
+      "stile-container-monitor": "stile-container-monitor",
+      "registrator-docker-image": "registrator-docker-image"
+    },
+    "amis": {
+      "base-ami": "base-iamge",
+      "base-ami:ap-southeast-2": "base-ami:ap-southeast-2",
+      "base-ami:us-west-2": "base-ami:us-west-2"
+    }
+  })
 }
 
 # Returns all coffees
@@ -17,6 +36,6 @@ output "all" {
   value = data.stile_manifest.all
 }
 
-output "excel_exporter_image" {
-  value = data.stile_manifest.all.service_versions["stile-excel-exporter-docker-image"]
+output "image" {
+  value = data.stile_manifest.all.service_versions["stile-prober"]
 }
